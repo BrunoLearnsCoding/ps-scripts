5081a3c10acc66cc8993a4417bbdae1ac2aecd2e
BrunoLearnsCoding <bruno@ilearncoading.com>
2024-05-26 12:07:26 +0330

    Improving highlight-string function

--- Scripts/highlight-string.ps1
+++ Scripts/highlight-string.ps1
@@ -1,22 +1,31 @@
-function Highlight-String {
+function Format-MatchString {
     param(
-        [Parameter(Mandatory=$true)]
+        [switch]$CaseSensitive,
+        [Parameter(Mandatory = $true)]
         [string]$String,
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory = $true)]
         [string]$Pattern,
         [string]$Color = 'Green'
     )
 
-    $match = [regex]::Match($String, $Pattern)
-
-    if ($match.Success) {
+    # if ($CaseSensitive -ne $true) {
+    #     $option = 
+    # }
+    $match = [regex]::Match($String, $Pattern, [System.Text.RegularExpressions.RegexOptions]::IgnoreCase)
+    while ($match.Success) {
         $before = $String.Substring(0, $match.Index)
         $after = $String.Substring($match.Index + $match.Length)
-
+    
         Write-Host $before -NoNewline
         Write-Host $match.Value -NoNewline -ForegroundColor $Color
-        Write-Host $after
-    } else {
-        Write-Host $String
+    
+        # Update the string to the remaining part after the match
+        $String = $after
+    
+        # Find the next match in the remaining string
+        $match = [regex]::Match($String, $Pattern)
     }
+    Write-Host $String
 }
+
+Format-MatchString -String 'Hello World in second World war' -Pattern 'World' -Color 'Red'
--- script-to-see-how-to-debug-powershell.ps1
+++ script-to-see-how-to-debug-powershell.ps1
@@ -1,15 +1,51 @@
-$files = @(
-    @{
-        name='Hasan' 
-        age = 65
-    }
-    @{
-        name='Ali' 
-        age= 41
-    }
+# $files = @(
+#     @{
+#         name='Hasan' 
+#         age = 65
+#     }
+#     @{
+#         name='Ali' 
+#         age= 41
+#     }
 
-)
+# )
 
-foreach ($file in $files) {
-    Write-Host $file.name
+# foreach ($file in $files) {
+#     Write-Host $file.name, $file.age
+# }
+
+function Get-RectangleArea {
+    [CmdletBinding()]
+    param (
+        # Rectengle height
+        [Parameter(Mandatory, ValueFromPipeline)]
+        [int]
+        $Height,
+        # Rectengle Width
+        [Parameter(Mandatory)]
+        [int]
+        $Width
+    )
+    
+    begin {
+        $Area = 0
+        Write-Verbose "Starting calculation"
+
+    }
+    
+    process {
+        if ($Width -le 0 -or $Height -le 0) {
+            Write-Error "Width and Height must be greater than 0"
+            return
+        }
+        # Initialize the area
+        $Area = $Width * $Height
+        Write-Host $Area
+    }
+    
+    end {
+        Write-Verbose "Calculation completed"
+    }
 }
+
+14, 12 | Get-RectangleArea -Width 5 -Verbose
