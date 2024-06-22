6f37ec629ef9d4f7f3a596aaf1a183072d4e68fa
BrunoLearnsCoding <bruno@ilearncoading.com>
2024-06-22 14:49:24 +0330

    Install-Winget and Install-Git functions written

--- Scripts/Install-DevelopmentTools.ps1
+++ Scripts/Install-DevelopmentTools.ps1
@@ -1,26 +1,43 @@
-# $programName = "git"
+function Check-ProgramInstalled {
+    param (
+        [string]$programName
+    )
+    try {
+        $programPath = Get-Command $programName
+        Write-Host "$programName is installed at: $($programPath.Source)"
+    } 
+    catch {
+        Write-Host "$programName is not installed."
+    }
+}
 
-# $installedPrograms = Get-WmiObject -Class Win32_Product | Select-Object -ExpandProperty Name 
+# Example usage:
 
-# foreach ($installedProgram in $installedPrograms) {
-#     if ($installedProgram -match $programName) {
-#         Write-Host "$installedProgram is installed."
-#     }
-# }
-# a function to install winget cli
 function Install-Winget {
+    $wingitInstalled = Check-ProgramInstalled "winget"
+    if ($wingitInstalled -eq $true) {
+        Write-Host "Winget is installed."
+        return
+    }
     $releaseUrl =  "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
     $releaseData = Invoke-RestMethod -Uri $releaseUrl
-    $assets = $ReleaseData.assets | Where-Object { $_.name -eq "Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" }
+    $wingetAsset = $ReleaseData.assets | Where-Object { $_.name -eq "Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" }
 
-    $assets
-
-    foreach ($asset in $assets) {
-        Invoke-WebRequest -Uri $asset.browser_download_url -OutFile "$($Env:TEMP)\$($asset.name)" 
-    }
-
+    $fileName = "$($Env:TEMP)\$($wingetAsset.name)"
+    Invoke-WebRequest -Uri $wingetAsset.browser_download_url -OutFile $fileName 
+    Add-AppxPackage -Path $fileName -Register
 }
 Install-Winget
 
+function Install-Git {
+    $gitInstalled = Check-ProgramInstalled "git"
+    if ($gitInstalled -eq $true) {
+        Write-Host "Git is installed."
+        return
+    }
+
+    winget install --id Git.Git -e --source winget
+}
+Install-Git
 
 
