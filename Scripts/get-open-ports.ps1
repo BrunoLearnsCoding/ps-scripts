f10f466ad9f91eb702738f2e4ee48468dbaed074
BrunoLearnsCoding <bruno@ilearncoading.com>
2024-08-04 09:46:15 +0330

    Get-OpenPort function created and tested

---
+++ Scripts/get-open-ports.ps1
@@ -0,0 +1,33 @@
+function Get-OpenPorts {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$ComputerName,
+        
+        [Parameter(Mandatory=$true)]
+        [int]$StartPort,
+        
+        [ValidateRange(0, 65536)]
+        [int]$EndPort
+    )
+
+    if (-not $EndPort) {
+        $EndPort = $StartPort
+    }
+
+    if ($StartPort -gt $EndPort) {
+        Write-Host "StartPort must be less than EndPort"
+        return
+    }
+
+    $PortRange = $StartPort..$EndPort
+
+    foreach ($port in $PortRange) {
+        $result = Test-NetConnection -ComputerName $ComputerName -Port $port -InformationLevel Quiet
+
+        if ($result -eq $true) {
+            Write-Host "Port $port is open on $ComputerName"
+        }
+    }
+}
+
+Get-OpenPorts -ComputerName 8.8.8.8 -StartPort 447 -EndPort 400
