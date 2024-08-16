236acc97cc61dcb119337fce58ddfc62234607b8
BrunoLearnsCoding <bruno@ilearncoading.com>
2024-08-16 18:16:01 +0330

    Buggy line commented

--- Scripts/exchange-certificates.ps1
+++ Scripts/exchange-certificates.ps1
@@ -7,6 +7,6 @@ $txtrequest = New-ExchangeCertificate -PrivateKeyExportable $True -GenerateReque
 
  
 
-Import-ExchangeCertificate -FileData ([System.IO.File]::ReadAllBytes('<FilePathOrUNCPath>')) [-Password (ConvertTo-SecureString -String '<Password> ' -AsPlainText -Force)] [-PrivateKeyExportable <$true | $false>] [-Server <ServerIdentity>]
+# Import-ExchangeCertificate -FileData ([System.IO.File]::ReadAllBytes('<FilePathOrUNCPath>')) [-Password (ConvertTo-SecureString -String '<Password> ' -AsPlainText -Force)] [-PrivateKeyExportable <$true | $false>] [-Server <ServerIdentity>]
 
 Import-ExchangeCertificate -FileData ([System.IO.File]::ReadAllBytes('\\mail-01\CSRs\2023\taamlocomotive2023-Final.CER')) -Server mail-01 -Password (Get-Credential).password -Wahtif
